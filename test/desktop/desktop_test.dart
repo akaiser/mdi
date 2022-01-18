fb4df087a0fd8c662139e64940d669c47fb3f8b6
Albert Kaiser <ak@kaiserv.de>
2022-01-18 16:21:44 +0100

    testing test workflow

---
+++ test/desktop/desktop_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/foundation.dart' show Key;
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mdi/_data.dart';
+import 'package:mdi/desktop/desktop.dart';
+import 'package:mdi/desktop/desktop_items.dart';
+import 'package:mdi/desktop/dock.dart';
+import 'package:mdi/window/window.dart';
+
+import '../widget_tester.dart';
+
+void main() {
+  group('Desktop', () {
+    testWidgets(
+        'renders $DesktopItems but no $Window and $Dock '
+        'if there are no windows opened', (tester) async {
+      await tester.pumpWidgetEx(
+        const Desktop(
+          groupedApps: groupedApps,
+          standaloneApps: standaloneApps,
+        ),
+      );
+
+      expect(find.byType(DesktopItems), findsOneWidget);
+      expect(find.byType(Window), findsNothing);
+      expect(find.byType(Dock), findsNothing);
+    });
+
+    testWidgets(
+        'renders $DesktopItems and one $Window and $Dock '
+        'if one desktop item was tapped', (tester) async {
+      await tester.pumpWidgetEx(
+        const Desktop(
+          groupedApps: groupedApps,
+          standaloneApps: standaloneApps,
+        ),
+      );
+
+      await tester.tap(find.byKey(const Key('games')));
+      await tester.pump();
+
+      expect(find.byType(DesktopItems), findsOneWidget);
+      expect(find.byType(Window), findsOneWidget);
+      expect(find.byType(Dock), findsOneWidget);
+    });
+  });
+}
