e66dfd2265e96a7130f4a96c104220807a7e9a5d
Albert Kaiser <ak@kaiserv.de>
2022-03-20 21:04:53 +0100

    fix build

--- lib/apps/manual_count.dart
+++ lib/apps/manual_count.dart
@@ -12,7 +12,6 @@ class _ManualCountState extends State<ManualCount> {
 
   @override
   Widget build(BuildContext context) {
-    print('###### BUILD Manual Count');
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       mainAxisSize: MainAxisSize.min,
