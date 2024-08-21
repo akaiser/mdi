493062f65ec4b3c0383ccfd9814746f4c465489a
Albert Kaiser <ak@kaiserv.de>
2024-08-21 20:34:59 +0200

    flutter 3.24.1

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,12 +1,10 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-const loadingEl = document.querySelector('#loading');
-
 _flutter.loader.load({
-    onEntrypointLoaded: async function (engineInitializer) {
-        const appRunner = await engineInitializer.initializeEngine({canvasKitBaseUrl: "canvaskit/"});
-        await appRunner.runApp();
-        loadingEl.remove();
-    }
+  onEntrypointLoaded: async function (engineInitializer) {
+    const appRunner = await engineInitializer.initializeEngine();
+    await appRunner.runApp();
+    document.querySelector('#loading').remove();
+  }
 });
